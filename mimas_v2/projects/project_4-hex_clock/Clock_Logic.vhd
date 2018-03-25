b579dd53dae4848e0339a0b358ed636035c989af
Jacob Zarnstorff <jakez03@hotmail.com>
2018-03-25 15:06:18 -0500

    Cleaning up clock logic code

--- mimas_v2/projects/project_4-hex_clock/Clock_Logic.vhd
+++ mimas_v2/projects/project_4-hex_clock/Clock_Logic.vhd
@@ -16,8 +16,13 @@ end Clock_Logic;
 
 architecture RTL of Clock_Logic is
 
-    signal hrs : integer range 0 to 12 := 12;
-    signal sec, min : integer range 0 to 60 := 0;
+    type t_Clock is record
+        hours : integer range 0 to 12;
+        minutes : integer range 0 to 60;
+        seconds : integer range 0 to 60;
+    end record t_Clock;
+    
+    signal s_Clock : t_Clock := (12, 0, 0);
     signal r_Count : integer := 1;
     signal w_Clk_1Hz : std_logic := '0';
 
@@ -37,29 +42,29 @@ begin
     p_Real_Clock : process(w_Clk_1Hz, i_Btn_Min, i_Btn_Hrs)
     begin
         if rising_edge(w_Clk_1Hz) then
-            sec <= sec + 1;
-            if (sec = 59 or i_Btn_Min = '0') then
-                sec <= 0;
-                min <= min + 1;
-                if (min >= 59) then
-                    min <= 0;
-                    hrs <= hrs + 1;
-                    if (hrs >= 12) then
-                        hrs <= 1;
+            s_Clock.seconds <= s_Clock.seconds + 1;
+            if (s_Clock.seconds = 59 or i_Btn_Min = '0') then
+                s_Clock.seconds <= 0;
+                s_Clock.minutes <= s_Clock.minutes + 1;
+                if (s_Clock.minutes >= 59) then
+                    s_Clock.minutes <= 0;
+                    s_Clock.hours <= s_Clock.hours + 1;
+                    if (s_Clock.hours >= 12) then
+                        s_Clock.hours <= 1;
                     end if;
                 end if;
         elsif (i_Btn_Hrs = '0') then
-            hrs <= hrs + 1;
-            sec <= 0;
-                if (hrs >= 12) then
-                    hrs <= 1;
+            s_Clock.hours <= s_Clock.hours + 1;
+            s_Clock.seconds <= 0;
+                if (s_Clock.hours >= 12) then
+                    s_Clock.hours <= 1;
                 end if;
             end if;
         end if;
     end process p_Real_Clock;
 
-    o_Seconds <= std_logic_vector(to_unsigned(sec, o_Seconds'length));
-    o_Minutes <= std_logic_vector(to_unsigned(min, o_Minutes'length));
-    o_Hours <= std_logic_vector(to_unsigned(hrs, o_Hours'length));
+    o_Seconds <= std_logic_vector(to_unsigned(s_Clock.seconds, o_Seconds'length));
+    o_Minutes <= std_logic_vector(to_unsigned(s_Clock.minutes, o_Minutes'length));
+    o_Hours <= std_logic_vector(to_unsigned(s_Clock.hours, o_Hours'length));
 
 end architecture RTL;
